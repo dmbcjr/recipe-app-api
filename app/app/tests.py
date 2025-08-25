54272ddfc668974ed7e9925d9cd7e0a452fffb83
dmbcjr <dcurranjr@gmail.com>
2025-08-25 21:29:04 +0100

    Added Psycopg2

---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+Sample tests for the Recipe application.
+
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Tests for the calc module."""
+
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers."""
+        res = calc.subtract(10, 8)
+        self.assertEqual(res, 2)
